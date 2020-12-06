a35f11e63310c8e96cfece75a1dbf6887df386f2
Matusek Márton <matusek.marton@autsoft.hu>
2020-12-06 09:11:37 +0100

    Test data preparation and prediction

---
+++ test_prep_and_predict_Test2.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+df = pd.read_csv("./data/Test2NER.csv", sep=";", names=["Sentence #", "Word"], encoding = "latin1")
+df.fillna('', inplace=True)
+
+def fill_sentence_numbers(df):
+    sentences = df["Sentence #"].values
+    current = sentences[0]
+    new_sentence_nums = []
+    for s in sentences:
+        if s != '':
+            current = s
+        new_sentence_nums.append(int(current.replace("Sentence: ", "")))
+    df["Sentence_num"] = new_sentence_nums
+        
+fill_sentence_numbers(df)
+
+sentences = df.groupby('Sentence_num').apply(lambda row: " ".join(row["Word"]))
+
+from flair.models import SequenceTagger
+from flair.data import Sentence
+
+model = SequenceTagger.load('final-model.pt')
+
+tagged_sentences = []
+# create example sentence
+for sentence_string in sentences:
+    sentence = Sentence(text=sentence_string, use_tokenizer=False)
+
+    # predict
+    model.predict(sentence)
+    tagged_sentences.append(sentence.to_tagged_string())
+
+import re
+ner_regex = re.compile('^<[B|I]-.+>')
+
+# set all predictions to O
+df["Predicted"] = "O"
+
+row_index = 0
+for tagged_sentence in tagged_sentences:
+    sentence_tokens = tagged_sentence.split(" ")
+    for token in sentence_tokens:
+        if ner_regex.match(token) is not None:
+            # set previous word tag
+            df.iat[row_index - 1, 3] = token[1:-1]
+        else:
+            row_index += 1
+print(row_index)
+print(len(df))
+
+df.to_csv("Test2NER_Prediction.csv", sep=";", index=False, columns=["Sentence #", "Word", "Predicted"], header=["Sentences", "Word", "Predicted"])
